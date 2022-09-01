d56b1b2741dcb94ebc03a81e3b636ed110a6c6b4
Pierre Sköldborg <pierre@progster.se>
2022-09-01 19:47:37 +0200

    Fix image path

--- src/components/HeroGraphics/HeroGraphics.tsx
+++ src/components/HeroGraphics/HeroGraphics.tsx
@@ -65,7 +65,7 @@ const HeroGraphics: React.FC<HeroGraphicsProps> = ({ visible }) => {
 						<rect x="0" y="0" width="100%" height="100%" fill="#000"></rect>
 						<g transform="translate(-1200,0)">
 							<image
-								href={`${assetsPrefix}/stars.jpg`}
+								href={`/${assetsPrefix}/stars.jpg`}
 								x="100%"
 								y="50"
 								width="1200"
